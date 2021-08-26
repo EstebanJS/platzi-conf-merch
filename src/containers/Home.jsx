92d869162c5737a3f110adef4682ae9b7004a6f0
Esteban Jimenez <yeisson310@hotmail.com>
2021-08-26 18:28:16 -0500

    [add] react-helmet

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import {Helmet} from 'react-helmet';
 import initialState from '../initialState';
 import Products from '../components/Products';
 
 const Home = () => (
   <>
+    <Helmet>
+      <title>Platzi Conf Merch - Proudcts</title>
+      <meta name="description" content="Store of cool Platzi things " />
+    </Helmet>
     <Products products={initialState.products} />
   </>
 );
